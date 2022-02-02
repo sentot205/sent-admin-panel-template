8a94ce4ade3a5b427a245984dc11c135f6dd6f17
sentot205 <sentot205@gmail.com>
2022-02-02 20:28:57 +0700

    Add Page Blank-

---
+++ src/components/blank.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Blank() {
+  return (
+    <>
+      <div className="container mx-auto bg-white dark:bg-gray-900 h-full">
+        <div class="dark:bg-gray-900 bg-gray-50 px-3 md:px-8 h-screen"/>
+      </div>
+    </>
+  );
+}
+
+export default Blank;
