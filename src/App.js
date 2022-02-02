982455a289f7ddf7834ca53634ef2a699eb986e9
sentot205 <sentot205@gmail.com>
2022-02-02 20:30:50 +0700

    Delete Default Component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import Sidebar from "./components/sidebar";
 import Navbar from "./components/navbar";
 // import Login from "./components/login";
 import Tables from "./components/table";
+// import Blank from "./components/blank";
 import Footer from "./components/footer";
-
-// import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
 
@@ -15,13 +13,6 @@ function App() {
       <Navbar />
       <Sidebar />
       <div className="ml-0 md:ml-64 App">
-        {/* <header className="App-header bg-white dark:bg-gray-800 text-gray-800 dark:text-white transition delay-150 duration-300">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header> */}
-        {/* <Login /> */}
         <Tables/>
         <Footer />
       </div>
